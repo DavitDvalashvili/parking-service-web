0e864aec3954e0fbfcf616ba39b66936e2569108
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-12-05 18:43:43 +0400

    feat: install zustand and and global functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import "@fontsource/firago";
+import { create } from "zustand";
+
+// Language store
+type UseLanguage = {
+  language: string;
+  toggleLanguage: (language: string) => void;
+};
+
+// Theme store
+type UseTheme = {
+  theme: string;
+  toggleTheme: (theme: string) => void;
+};
+
+export const useLanguage = create<UseLanguage>((set) => ({
+  language: "Ge",
+  toggleLanguage: (language: string) => set({ language }),
+}));
+
+export const UseTheme = create<UseTheme>((set) => ({
+  theme: "Light",
+  toggleTheme: (theme: string) => set({ theme }),
+}));
+
 const App = () => {
-  return <div className="bg-green-600">dvala</div>;
+  return <div className="font-firago">შეტყობინება</div>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        firago: ["Firago", "sans-serif"],
+      },
+      colors: {},
+      borderRadius: {},
+      backgroundImage: {},
+      boxShadow: {},
+    },
   },
   plugins: [],
-}
-
+};
