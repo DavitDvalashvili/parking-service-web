479cf240afc5f1fe24a113681e494cd53b0d4439
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-12-10 14:26:50 +0400

    feat: complete services section for mobile

---
+++ src/components/Home/Services.tsx
@@ -0,0 +1,60 @@
+import data from "../../assets/locales/translations.json";
+import { useState } from "react";
+
+import { useLanguage } from "../../App";
+
+export const Services = () => {
+  const { language } = useLanguage();
+  const [activeService, setActiveService] = useState<number | undefined>(
+    undefined
+  );
+
+  const serviceContent = data[language].services;
+
+  return (
+    <section className="bg-white text-black dark:bg-dark-brown dark:text-white px-8 font-firago font-bold  uppercase ">
+      <h3 className="pt-[0.7rem] pb-[2rem] text-center text-[2rem]">
+        {serviceContent.title}
+      </h3>
+      <div className="flex flex-col gap-4">
+        {serviceContent.services.map((service, index) => (
+          <div
+            key={index}
+            className="border-[0.08rem] border-black dark:border-white rounded-[2rem] py-2 px-4"
+            onClick={() =>
+              setActiveService(activeService === index ? undefined : index)
+            }
+          >
+            <div className="flex justify-start items-center gap-[1.2rem] ">
+              <div
+                className="h-[6rem] w-[8.4rem] bg-no-repeat bg-auto bg-left "
+                style={{ backgroundImage: `url(${service.image})` }}
+              ></div>
+              <h6 className="text-[1.7rem] max-w-[21.3rem]">
+                {service.service}
+              </h6>
+            </div>
+            {activeService === index && (
+              <>
+                <div
+                  className={`font-bold text-[1.4rem]  ${
+                    service.description ? "mt-[1.1rem] mb-[0.7rem]" : ""
+                  } `}
+                >
+                  {service.description}
+                </div>
+                <p
+                  className={`font-normal text-[1.3rem]  ${
+                    service.text ? "mb-[1.1rem]" : ""
+                  }`}
+                >
+                  {service.text}
+                </p>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import data from "../assets/locales/translations.json";
+import { Services } from "../components/Home/Services";
 import { useLanguage } from "../App";
+import { WhyChooseUs } from "../components/Home/WhyChooseUs";
 
 export const Home = () => {
   const { language } = useLanguage();
@@ -8,12 +10,14 @@ export const Home = () => {
 
   return (
     <div>
-      <div className="font-bold text-[2rem] font-firago font-feature pt-4 text-black dark:text-white dark:bg-dark-brown">
+      <section className="font-bold text-[2rem] font-firago font-feature pt-4 text-black dark:text-white dark:bg-dark-brown">
         <h2 className="mx-auto text-center uppercase max-w-[26.1rem]">
           {coverContent.title}
         </h2>
         <div className="w-full h-[26.3rem] bg-mobile-light dark:bg-mobile-dark bg-cover bg-top border-t-[0.001rem] border-white dark:border-dark-brown"></div>
-      </div>
+      </section>
+      <Services />
+      <WhyChooseUs />
     </div>
   );
 };
