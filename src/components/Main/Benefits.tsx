d8b9cd93efd089b64f235ec76e73ae5f287f7994
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-12-19 21:19:22 +0400

    feat: complete benefit component desktop version

--- src/components/Main/Benefits.tsx
+++ src/components/Main/Benefits.tsx
@@ -10,7 +10,7 @@ export const Benefits = () => {
   return (
     <div
       className="bg-white dark:bg-dark-brown text-black font-firago pt-8 scroll-mt-[8.1rem] 
-      xl:pt-10 pl-[1.6rem] 
+      xl:pt-10 pl-[1.6rem] lg:pl-[6rem] xl:pl-[10rem]
       "
       id="about"
     >
@@ -21,12 +21,12 @@ export const Benefits = () => {
       xl:mt-[1.6rem] xl:mb-16 xl:text-[2rem]">
         {benefitContent.description}
       </p>
-      <Swiper slidesPerView={"auto"} spaceBetween={0} className="pr-[1.6rem]" >
+      <Swiper slidesPerView={"auto"} spaceBetween={0} className="pr-[1.6rem]  lg:pr-[5.6rem] xl:ml-[-2.5rem] xl:pr-[7.5rem]" >
         {benefitContent.benefits.map((benefit, index) => (
           <SwiperSlide key={index}>
             <div className="w-[20.8rem] h-[18.7rem] rounded-[1rem] bg-benefit-card bg-center bg-cover flex flex-col justify-between items-center p-2 pt-6 mx-[0.4rem] 
-              lg:w-[30.8rem] lg:h-[28.2rem]  lg:mx-[1rem] lg:p-[1.9rem] lg:pt-8 xl:w-[37.2rem] xl:h-[33.9rem] xl:mx-[2.7rem] xl:p-[2.2rem] xl:pt-[2.383rem] xl:justify-start
-              cursor-pointer">
+              lg:w-[30.8rem] lg:h-[28.2rem]  lg:mx-[1rem] lg:p-[1.9rem] lg:pt-8 xl:w-[37.2rem] xl:h-[33.9rem] xl:mx-[2.5rem] xl:p-[2.2rem] xl:pt-[2.383rem] xl:justify-start
+              cursor-pointer ">
               <img src="./images/benefits/parking.svg" alt="parkingLogo" />
               <p className="text-center font-medium text-[1.1rem] py-[0.625rem] leading-[1.3rem] lg:text-[1.7rem] lg:leading-8 xl:text-[1.8rem] xl:leading-[2.2rem] xl:pt-[4.64rem]">
                 {benefit.text}
